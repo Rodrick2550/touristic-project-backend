93952742c2c2c87d7fca523f1627a99e39885ac2
rodrick <213480@ids.upchiapas.edu.mx>
2023-04-17 18:52:10 -0600

    Create project and add dependencies

---
+++ src/main/java/com/example/touristicproject/TouristicProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristicProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristicProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/touristicproject/TouristicProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristicProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
